6a706639ddf963d37c1441dd18404de74800c6f1
Shubhankar0110 <edushubhankar@gmail.com>
2025-09-07 18:28:23 +0530

    IC

--- Lecture_27_Recursion/PrintNtoOne.java
+++ Lecture_27_Recursion/PrintNtoOne.java
@@ -1,16 +1,17 @@
 package Lecture_27_Recursion;
 
-public class PrintAllNums {
-
-    static void printNums(int n){
+public class PrintNtoOne {
+    static void printNum(int n){
+        if(n>=1){
         if(n==1){
             System.out.println(n);
             return;
         }
-        printNums(n-1);
-        System.out.println(n);  //self work
+        System.out.println(n);
+        printNum(n-1);
+    }
     }
     public static void main(String[] args) {
-        printNums(10);
+        printNum(10);
     }
 }
