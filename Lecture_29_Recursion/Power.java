36dcec713b2a8720f0feae4f8e7cbf9796dd8baf
Shubhankar0110 <edushubhankar@gmail.com>
2025-09-11 11:14:00 +0530

    Better Complexity

--- Lecture_29_Recursion/Power.java
+++ Lecture_29_Recursion/Power.java
@@ -1,12 +1,22 @@
 package Lecture_29_Recursion;
 
 public class Power{
-    static long pow(int p, int q){
+    static int OGpow(int p,int q){  //O(q) complexity
         if(q==0)
             return 1;
-        return p*pow(p, q-1);
+        return p*OGpow(p, q-1);
+    }
+    static int pow(int p, int q){   //Better complexity
+        if(q==0)
+            return 1;
+        int smallPow = pow(p, q/2);
+        if(q%2==0){
+            return smallPow * smallPow;
+        }
+        return smallPow * smallPow * p;
     }
     public static void main(String[] args) {
         System.out.println(pow(2,3));
+        System.out.println(OGpow(2, 5));
     }
 }
