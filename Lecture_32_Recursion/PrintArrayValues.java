15b9987843ad90fe0638b321ed92a0611a176935
Shubhankar0110 <edushubhankar@gmail.com>
2025-09-12 20:13:45 +0530

    Both Process Included

--- Lecture_32_Recursion/PrintArrayValues.java
+++ Lecture_32_Recursion/PrintArrayValues.java
@@ -1,6 +1,14 @@
 package Lecture_32_Recursion;
 
 public class PrintArrayValues {
+    static void reverseWayOfPrint(int[] arr,int idx){
+        if(idx==arr.length){
+            return;
+        }
+        System.out.print(arr[idx]+" ");
+        reverseWayOfPrint(arr, idx+1);
+    }
+    
     static void printArrays(int[] arr,int idx){
         if(idx==0){
             System.out.print(arr[0]+" ");
@@ -11,7 +19,15 @@ public class PrintArrayValues {
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,8,9,10};
+
+        System.out.println("Mine process: ");
         int idx = arr.length - 1;
         printArrays(arr,idx);
+
+        System.out.println();
+
+        System.out.println("Tutorial Process: ");
+        int idx1 = 0;
+        reverseWayOfPrint(arr,idx1);
     }
 }
