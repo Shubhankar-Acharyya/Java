86ba0a6db04352dc8f4f9534f3f7531e51989dd2
Shubhankar0110 <edushubhankar@gmail.com>
2025-09-16 12:13:47 +0530

    Added all index printing if present

--- Lecture_33_Recursion/FindNum.java
+++ Lecture_33_Recursion/FindNum.java
@@ -1,6 +1,18 @@
 package Lecture_33_Recursion;
 
 public class FindNum {
+    static void findIndex(int[] arr,int target,int idx){
+        //Base case
+        if(idx>arr.length-1){
+            return;
+        }
+        //Self Work
+        if(arr[idx]==target){
+            System.out.print(idx+" ");
+        }
+        //Recursive Work
+        findIndex(arr, target, idx+1);
+    }
     static boolean find(int[] arr,int target,int idx){
         //Base Case
         if(idx>arr.length-1){
@@ -14,12 +26,17 @@ public class FindNum {
         return find(arr, target, idx+1);
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,8,7,6,5};
-        int target = 5;
+        int[] arr = {1,2,8,7,6,2,5};
+        int target = 2;
+
+        //Just find if present or not
         if(find(arr, target, 0) == true){
             System.out.println("Yes it is present.");
         }
         else
             System.out.println("Not present.");
+        
+        //Find indexes if present
+        findIndex(arr, target, 0);
     }
 }
