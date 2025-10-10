cf302ac47339114ac5e393d2b5ef8f818224beb4
Shubhankar0110 <edushubhankar@gmail.com>
2025-10-10 10:18:17 +0530

    Added Best Approach

--- Lecture_40_Sorting_Problems/MoveZeros.java
+++ Lecture_40_Sorting_Problems/MoveZeros.java
@@ -1,7 +1,21 @@
 package Lecture_40_Sorting_Problems;
 
-public class MoveZeros {
-    static void moveZero(int[] arr){
+public class MoveZeros {    
+    static void moveZeroBestApproach(int[] arr) {   //O(n^2) complexity
+        int n = arr.length;
+        int j = 0; // points to position of first zero
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+    static void moveZero(int[] arr){    //O(n^2) Complexity
         int n = arr.length;
 
         for(int i = 0; i < n; i++){
@@ -17,7 +31,7 @@ public class MoveZeros {
     }
     public static void main(String[] args) {
         int[] arr = { 0,5,0,3,4,2};
-        moveZero(arr);
+        moveZeroBestApproach(arr);
         for(int i:arr){
             System.out.print(i+" ");
         }
